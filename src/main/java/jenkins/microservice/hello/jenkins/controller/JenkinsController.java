4806b1128bee15bcfad46060f2b25f399c636e29
faheemjan5000 <faheemjan500@gmail.com>
2024-04-06 02:12:17 +0200

    first commit

---
+++ src/main/java/jenkins/microservice/hello/jenkins/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package jenkins.microservice.hello.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @GetMapping("/hello")
+    public String greetingsJenkins(){
+        return "Welcome to jenkins";
+    }
+}
